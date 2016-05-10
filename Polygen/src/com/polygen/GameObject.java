2e5f0d99fd3b144fe56292aedf9a048f8d647e88
Christopher <Christopher@192.168.2.17>
2016-05-09 20:14:18 -0400

    added basic structer to game object

--- Polygen/src/com/polygen/GameObject.java
+++ Polygen/src/com/polygen/GameObject.java
@@ -1,5 +1,13 @@
 package com.polygen;
 
-public class GameObject {
-
+public abstract class GameObject {
+	private String[] tags;
+	
+	public GameObject(String[] tags[]){
+		this.tags = tags;
+	}
+	
+	public abstract void render();
+	
+	public abstract void update();
 }
