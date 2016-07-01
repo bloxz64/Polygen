7002a2569e0cda3431ba3b7a5eda20cceeb671e6
bloxz64 <bloxz64@gmail.com>
2016-07-01 17:37:30 -0400

    here

--- Polygen/src/com/flappy/BoardVeiw.java
+++ Polygen/src/com/flappy/BoardVeiw.java
@@ -1,5 +1,41 @@
 package com.flappy;
 
-public class BoardVeiw {
+import java.awt.Graphics;
+
+import com.polygen.*;
+
+public class BoardVeiw extends BasicState {
+
+	@Override
+	public void init(MainGame game) {		
+	}
+	
+	public void init(MainGame game, String level){
+		//load level
+	}
+
+	@Override
+	public void startState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g, MainGame game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(double delta, MainGame game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stopState() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
